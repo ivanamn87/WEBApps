4c07a577d2d7cd19f23042ad0b8c176701206b4b
ivanamn87 <49204016+ivanamn87@users.noreply.github.com>
2021-08-07 12:23:27 +0200

    adding figletProject exercise

---
+++ figletProject/main.js
@@ -0,0 +1,42 @@
+let figlet = require('figlet');
+
+
+/*
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+}); */
+
+
+figlet.fonts(function(err, fonts) {
+    if (err) {
+        console.log('something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.dir(fonts);
+});
+
+
+figlet.text('Hello world!', {
+    font: 'Avatar',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
+
+
+
+
